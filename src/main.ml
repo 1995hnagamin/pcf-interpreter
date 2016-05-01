74f3c412abb0897c7f54fd9b5023a6df7b71d113
Hideaki Nagamine <1995.hnagamin@gmail.com>
2016-05-02 01:32:32 +0900

    Type statically and show the type

--- src/main.ml
+++ src/main.ml
@@ -4,13 +4,17 @@ let eval_input buffer =
   let lexbuf = Lexing.from_string buffer in
   let ast = Parser.toplevel Lexer.token lexbuf in
   let Syntax.Exp p = ast in
-  Eval.eval Environment.initial_env p
+  let ty = Typing.eval_ty Environment.initial_env p in
+  let value = Eval.eval Environment.initial_env p in
+  (ty, value)
 
 let eval_and_stringify buffer =
   try
-    string_of_value (eval_input buffer)
+    let (ty, value) = eval_input buffer in
+    Printf.sprintf "- %s : %s" (string_of_ty ty) (string_of_value value)
   with
     Eval.Undefined -> "undef"
+  | Typing.Typing_error msg -> "Error: " ^ msg 
 
 let rec read_eval_print_loop _ =
   print_string "# ";
