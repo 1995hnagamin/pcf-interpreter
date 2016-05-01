6040c3195977562ddf3a8988861fb7cf4b36070d
Hideaki Nagamine <1995.hnagamin@gmail.com>
2016-05-02 08:08:59 +0900

    Exit on EoF

--- src/main.ml
+++ src/main.ml
@@ -18,11 +18,16 @@ let eval_and_stringify buffer =
   | Environment.Not_bound x -> "ERROR: Unbound variable: " ^ x
 
 let rec read_eval_print_loop _ =
-  print_string "# ";
-  let buffer = read_line () in
-  print_string (eval_and_stringify buffer);
-  print_newline ();
-  read_eval_print_loop ()
+  try
+    print_string "# ";
+    let buffer = read_line () in
+    print_string (eval_and_stringify buffer);
+    print_newline ();
+    read_eval_print_loop ()
+  with
+    End_of_file ->
+      print_newline ();
+      exit 0
 
 let () =
   read_eval_print_loop ()
